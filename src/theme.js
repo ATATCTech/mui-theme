2a25a511c1f93356a254b72d5357655d58dee3f1
ATATC <futerry@outlook.com>
2023-06-27 16:20:57 -0400

    Version `1.0.8`. Bug fixed.

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,11 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { requireStrictThemeMode, useSystemThemeMode, useThemeMode } from "./mode";
+import { requireStrictThemeMode, useSystemThemeMode } from "./mode";
 import { useMemo } from "react";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { defaultThemeConfig } from "./presets";
 export function Theme(props) {
-    const systemThemeConfig = useSystemThemeMode();
-    const themeMode = props.themeMode == null ? useThemeMode()[0] : requireStrictThemeMode(props.themeMode, systemThemeConfig);
+    const systemThemeMode = useSystemThemeMode();
+    const themeMode = props.themeMode == null ? systemThemeMode : requireStrictThemeMode(props.themeMode, systemThemeMode);
     const themeConfig = props.themeConfig == null ? defaultThemeConfig : props.themeConfig;
     const theme = useMemo(() => createTheme(themeConfig(themeMode)), [themeConfig]);
     return (_jsxs(ThemeProvider, { theme: theme, children: [_jsx(CssBaseline, {}), props.children] }));
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,12 +1,12 @@
 import {StrictThemeMode, ThemeConfig} from "./types";
-import {requireStrictThemeMode, useSystemThemeMode, useThemeMode} from "./mode";
+import {requireStrictThemeMode, useSystemThemeMode} from "./mode";
 import {ReactNode, useMemo} from "react";
 import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import {defaultThemeConfig} from "./presets";
 
 export function Theme(props: {readonly themeMode?: StrictThemeMode, readonly themeConfig?: ThemeConfig, readonly children: ReactNode}): ReactNode {
-    const systemThemeConfig = useSystemThemeMode();
-    const themeMode = props.themeMode == null ? useThemeMode()[0] : requireStrictThemeMode(props.themeMode, systemThemeConfig);
+    const systemThemeMode = useSystemThemeMode();
+    const themeMode = props.themeMode == null ? systemThemeMode : requireStrictThemeMode(props.themeMode, systemThemeMode);
     const themeConfig = props.themeConfig == null ? defaultThemeConfig : props.themeConfig;
     const theme = useMemo(() => createTheme(themeConfig(themeMode)), [themeConfig]);
     return (
