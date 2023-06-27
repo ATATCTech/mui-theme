71a370e41f15c9a211e93f999aac98a1f18671cf
ATATC <futerry@outlook.com>
2023-06-27 18:23:05 -0400

    Version `1.1.0`. Supported dynamic theme config.

--- src/theme.tsx
+++ src/theme.tsx
@@ -8,7 +8,7 @@ export function Theme(props: {readonly themeMode?: StrictThemeMode, readonly the
     const systemThemeMode = useSystemThemeMode();
     const themeMode = props.themeMode == null ? systemThemeMode : requireStrictThemeMode(props.themeMode, systemThemeMode);
     const themeConfig = props.themeConfig == null ? defaultThemeConfig : props.themeConfig;
-    const theme = useMemo(() => createTheme(themeConfig(themeMode)), [themeMode]);
+    const theme = useMemo(() => createTheme(themeConfig(themeMode)), [themeMode, themeConfig]);
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline/>
