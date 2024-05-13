7f2d30324e4a03fad3fab2a9ad800c1d64564034
ATATC <futerry@outlook.com>
2024-05-13 17:39:49 -0400

    Bug fixed: outdated APIs.

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,20 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import { jsx as _jsx } from "react/jsx-runtime";
 import { requireStrictThemeMode, useSystemThemeMode } from "./mode";
 import { useMemo } from "react";
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { defaultThemeConfig, themeConfigMapping } from "./presets";
 export function Theme(props) {
     const systemThemeMode = useSystemThemeMode();
     const themeMode = props.themeMode == null ? systemThemeMode : requireStrictThemeMode(props.themeMode, systemThemeMode);
     const themeConfig = props.themeConfig == null ? defaultThemeConfig : props.themeConfig;
     const theme = useMemo(() => createTheme(themeConfig(themeMode)), [themeMode, themeConfig]);
-    return (_jsxs(ThemeProvider, { theme: theme, children: [_jsx(CssBaseline, {}), props.children] }));
+    return (_jsx(ThemeProvider, { theme: theme, children: props.children }));
 }
 export function DynamicTheme(props) {
     const systemThemeMode = useSystemThemeMode();
     const themeMode = props.themeMode == null ? systemThemeMode : requireStrictThemeMode(props.themeMode, systemThemeMode);
     const themeConfig = props.themeConfigID == null ? defaultThemeConfig : themeConfigMapping(props.themeConfigID);
     const theme = useMemo(() => createTheme(themeConfig(themeMode)), [themeMode, themeConfig]);
-    return (_jsxs(ThemeProvider, { theme: theme, children: [_jsx(CssBaseline, {}), props.children] }));
+    return (_jsx(ThemeProvider, { theme: theme, children: props.children }));
 }
 //# sourceMappingURL=theme.js.map
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,7 +1,7 @@
 import {StrictThemeMode, ThemeConfig, ThemeConfigMapping} from "./types";
 import {requireStrictThemeMode, useSystemThemeMode} from "./mode";
 import {ReactNode, useMemo} from "react";
-import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
 import {defaultThemeConfig, themeConfigMapping} from "./presets";
 
 export function Theme(props: {themeMode?: StrictThemeMode, themeConfig?: ThemeConfig, children: ReactNode}): ReactNode {
@@ -11,7 +11,6 @@ export function Theme(props: {themeMode?: StrictThemeMode, themeConfig?: ThemeCo
     const theme = useMemo(() => createTheme(themeConfig(themeMode)), [themeMode, themeConfig]);
     return (
         <ThemeProvider theme={theme}>
-            <CssBaseline/>
             {props.children}
         </ThemeProvider>
     );
@@ -24,7 +23,6 @@ export function DynamicTheme(props: {themeMode?: StrictThemeMode, themeConfigID?
     const theme = useMemo(() => createTheme(themeConfig(themeMode)), [themeMode, themeConfig]);
     return (
         <ThemeProvider theme={theme}>
-            <CssBaseline/>
             {props.children}
         </ThemeProvider>
     );
